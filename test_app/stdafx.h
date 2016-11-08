94e365bd59320966c5a06f1cbc01d222229bb974
vasily-knk <vasily.knk@gmail.com>
2016-11-08 21:08:46 +0300

    test_app

---
+++ test_app/stdafx.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <SDKDDKVer.h>
+
+#include <vector>
+using std::vector;
+
+#include <utility>
+using std::pair;
+
+#include <boost/optional.hpp>
+using boost::optional;
+
+#include <boost/type_traits.hpp>
